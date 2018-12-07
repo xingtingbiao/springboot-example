2323e13988c819a8a01fcbc9b01b2f2a58088996
xingtingbiao <xingtingbiao@znlhzl.com>
2018-12-07 16:40:19 +0800

    feature: gradle多环境处理

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # springboot-example
 
+maven:多环境配置
+参考: http://www.cnblogs.com/zeng1994/p/06917ed3b98677fa7a1b0f74de16c3be.html
 mvn clean package -Dmaven.test.skip=true -Psit
-
 java -jar xxx.jar --spring.profiles.active=test 表示使用测试环境的配置
+
+gradle:多环境配置
+./gradlew clean build -Dprofile=sit
+参考: https://blog.csdn.net/liwgdx80204/article/details/80942440
---
+++ gradle-springboot/src/main/java/com/example/gradlespringboot/GradleSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.gradlespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSpringbootApplication.class, args);
+    }
+}
