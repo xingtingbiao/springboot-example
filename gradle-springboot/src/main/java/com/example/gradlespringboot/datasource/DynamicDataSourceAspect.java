a6d8fa9416cd6e14272e78c2c0289b3a7b3a5dde
xingtingbiao <18551617816@163.com>
2019-07-10 10:46:52 +0800

    Update DynamicDataSourceAspect.java

--- gradle-springboot/src/main/java/com/example/gradlespringboot/datasource/DynamicDataSourceAspect.java
+++ gradle-springboot/src/main/java/com/example/gradlespringboot/datasource/DynamicDataSourceAspect.java
@@ -4,16 +4,23 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
 @Component
 @Aspect
+@Order(1)
 public class DynamicDataSourceAspect {
+    @Pointcut("@annotation(com.gridx.akka_cluster_supervisor.datasource.DSAnnotation))")
+    public void dbPointCut() {
+    }
 
-    @Before("execution(* com.example.gradlespringboot.mapper..*.*(..))")
+    // @Before("execution(* com.example.gradlespringboot.mapper..*.*(..))")
+    @Before("dbPointCut()")
     public void before(JoinPoint point) {
         String dataSource = DataSourceContextHolder.DEFAULT_DS;
         DSAnnotation annotationOfClass = (DSAnnotation)point.getSignature().getDeclaringType().getAnnotation(DSAnnotation.class);
@@ -24,7 +31,8 @@ public class DynamicDataSourceAspect {
         DataSourceContextHolder.setDB(dataSource);
     }
 
-    @After("execution(* com.example.gradlespringboot.mapper..*.*(..))")
+    // @After("execution(* com.example.gradlespringboot.mapper..*.*(..))")
+    @After("dbPointCut()")
     public void after(JoinPoint point){
         DataSourceContextHolder.clearDB();
     }
